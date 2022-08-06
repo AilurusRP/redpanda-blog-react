6ac5044deb1bf2f3ea5b31cae5b5a7704b53763d
Red Panda <1148648777@qq.com>
2022-08-06 16:50:21 +0800

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,39 @@ import { ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 import ArticleListPage from "./views/article-list-page";
 import { redpandaTheme } from "./themes/redpanda-theme";
+import { Route, Routes } from "react-router-dom";
+import BlogLayout from "./components/UI/blog-layout";
+import BlogTagsList from "./components/article-list-page/blog-tags-list";
+import { useEffect, useState } from "react";
+import { BlogData } from "./interfaces/blog-data";
+import { getAllBlogs } from "./services/apis";
+import ArticleContentPage from "./views/article-content-page";
 
 export default function App() {
+    const [blogDataList, setBlogDataList] = useState<BlogData[]>([]);
+    useEffect(() => {
+        getBlogDataList(setBlogDataList);
+    }, []);
     return (
         <div className="App">
             <ThemeProvider theme={redpandaTheme}>
-                <ArticleListPage></ArticleListPage>
+                <BlogLayout
+                    headerText={"小熊猫的博客"}
+                    leftBoxList={<BlogTagsList value={blogDataList} />}
+                    contentList={
+                        <Routes>
+                            <Route path="/" element={<ArticleListPage value={blogDataList} />} />
+                            <Route path="/article" element={<ArticleContentPage />} />
+                        </Routes>
+                    }
+                />
             </ThemeProvider>
         </div>
     );
+}
+
+async function getBlogDataList(setBlogDataList: React.Dispatch<React.SetStateAction<BlogData[]>>) {
+    const blogDataList = await getAllBlogs();
+    console.log(blogDataList);
+    if (blogDataList) setBlogDataList(blogDataList);
 }
--- src/views/article-list-page.tsx
+++ src/views/article-list-page.tsx
@@ -1,26 +1,6 @@
-import { useEffect, useState } from "react";
-import BlogTagsList from "../components/article-list-page/blog-tags-list";
-import BlogLayout from "../components/UI/blog-layout";
 import { BlogData } from "../interfaces/blog-data";
-import { getAllBlogs } from "../services/apis";
 import BlogContentList from "../components/article-list-page/blog-content-list";
 
-export default function ArticleListPage() {
-    const [blogDataList, setBlogDataList] = useState<BlogData[]>([]);
-    useEffect(() => {
-        getBlogDataList(setBlogDataList);
-    }, []);
-    return (
-        <BlogLayout
-            headerText={"小熊猫的博客"}
-            leftBoxList={<BlogTagsList value={blogDataList} />}
-            contentList={<BlogContentList value={blogDataList} />}
-        />
-    );
-}
-
-async function getBlogDataList(setBlogDataList: React.Dispatch<React.SetStateAction<BlogData[]>>) {
-    const blogDataList = await getAllBlogs();
-    console.log(blogDataList);
-    if (blogDataList) setBlogDataList(blogDataList);
+export default function ArticleListPage(props: { value: BlogData[] }) {
+    return <BlogContentList value={props.value} />;
 }
