64733d3591aa19f476d160eb490aa079ac3bbe75
AilurusRP <AilurusRP@hotmail.com>
2023-07-19 22:09:56 +0800

    refactor: Migrate from webpack to vite.

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,3 +1,3 @@
 export default async function request<T>(resource: string, options?: RequestInit): Promise<T | undefined> {
-    return (await fetch(`/${resource}`, options)).json();
+    return (await fetch(`/api/${resource}`, options)).json();
 }
